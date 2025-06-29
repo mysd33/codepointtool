241ad7f780e628e2dbc62d4c3476c7bde32d3163
mysd33 <mysd.33@gmail.com>
2025-06-29 17:01:34 +0900

    ソースコメント生成内容の修正

--- src/main/java/codepointtool/TargetDataCreator.java
+++ src/main/java/codepointtool/TargetDataCreator.java
@@ -18,16 +18,16 @@ public class TargetDataCreator {
         return List.of(
                 // TODO: 必要に応じて追加・修正してください
                 // JIS X 0213で追加された非漢字の特殊ひらがな
-                new TargetData("JIS_X_0213_SpecialHiragana", "特殊ひらがな（1面4区）の文字集合を表すクラス",
+                new TargetData("JIS_X_0213_SpecialHiragana", "JIS X 0213の特殊ひらがな（1面4区）の文字集合を表すクラス",
                         jisCharacter -> "01".equals(jisCharacter.getMen()) && "04".equals(jisCharacter.getKu())
                                 && JISCharacter.JIS_ADDED_NOT_KANJI.equals(jisCharacter.getKubun())),
                 // JIS X 0213で追加された非漢字の特殊カタカナ
-                new TargetData("JIS_X_0213_SpecialKatakana", "特殊カタカナ（1面5区）の文字集合を表すクラス",
+                new TargetData("JIS_X_0213_SpecialKatakana", "JIS X 0213の特殊カタカナ（1面5区）の文字集合を表すクラス",
                         jisCharacter -> "01".equals(jisCharacter.getMen()) && "05".equals(jisCharacter.getKu())
                                 && JISCharacter.JIS_ADDED_NOT_KANJI.equals(jisCharacter.getKubun())),
                 // JIS X 0213で追加された非漢字の特殊文字、記号
                 new TargetData("JIS_X_0213_AddedSymbol",
-                        "各種記号（1面2-3区、6-7区、8区）、西欧系各種文字・記号（1面9-11区）、各種数字記号（12-13区）の文字集合を表すクラス",
+                        "JIS X 0213で追加された各種記号（1面2-3区、6-7区、8区）、西欧系各種文字・記号（1面9-11区）、各種数字記号（12-13区）の文字集合を表すクラス",
                         jisCharacter -> "01".equals(jisCharacter.getMen())
                                 && ("02".equals(jisCharacter.getKu()) || "03".equals(jisCharacter.getKu())
                                         || "06".equals(jisCharacter.getKu()) || "07".equals(jisCharacter.getKu())
